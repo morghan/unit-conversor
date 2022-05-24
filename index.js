3adf812bc4e15d6ea4a9b52860accb7c464fd62b
morghan <joel.hz24@gmail.com>
2022-05-24 12:33:25 -0600

    comments and refactoring

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 function convert() {
+	//Get the input's value
 	let number = document.getElementById('number').value;
+
 	let lengthParagraph = document.getElementById('length-conversion');
+
+	//Round the conversion to 3 decimal places
 	let metersToFeet = (number * 3.28084).toFixed(3);
 	let feetToMeters = (number * 0.3048).toFixed(3);
+
+	//Use conditional (ternary) operator to verify existence of input value
+	//to render accordingly into template literal
 	lengthParagraph.textContent =
 		`${number ? number : 0} meters = ${metersToFeet} feet | ` +
 		`${number ? number : 0} feet = ${feetToMeters} meters`;
 
+	//Rinse and repeat
 	let volumeParagraph = document.getElementById('volume-conversion');
 	let litersToGallons = (number * 0.264172).toFixed(3);
 	let gallonsToLiters = (number * 3.78541).toFixed(3);
